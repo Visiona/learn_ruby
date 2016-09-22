93364f478f2e4dac7dce96a79763ff7d41ffb1d3
Visiona <darbis@gmail.com>
2016-09-22 23:44:39 +0100

    Created Own Reverse Polish Calculator

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -59,7 +59,51 @@ class RPNCalculator
 
   def tokens(str)
     str = str.split(" ")
-    str.map {|x| "+ / - *".include? x ? x.to_sym : x.to_i}
+    str.map do |x| 
+      if "+ / - *".include? x
+        x.to_sym 
+      else
+        x.to_i
+      end
+    end
+  end
+
+  def evaluate(str_equation)
+    result= tokens(str_equation)
+    while result.length > 1
+      result.each_with_index do |x,i|
+        if x == :+
+          self.push(result[i-2]) # we add to sum method two numbers preceding :+ symbol
+          self.push(result[i-1])
+          self.plus # adding these two numbers
+          result[i] = self.value # adding the sum to the beginning of the array
+          result.slice!(i-2..i-1) # so we need to remove these two numbers and the symbol from the original array which is working array
+          break
+        elsif x == :-
+          self.push(result[i-2])
+          self.push(result[i-1])
+          self.minus
+          result[i] = self.value
+          result.slice!(i-2..i-1)
+          break
+        elsif x == :*
+          self.push(result[i-2])
+          self.push(result[i-1])
+          self.times
+          result[i] = self.value
+          result.slice!(i-2..i-1)
+          break
+        elsif x == :/
+          self.push(result[i-2])
+          self.push(result[i-1])
+          self.divide
+          result[i] = self.value
+          result.slice!(i-2..i-1)
+          break
+        end
+      end
+    end
+    result[0]
   end
 
 
