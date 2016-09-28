998c4db3f0b384e8ff954a5ee929e0dee30cb872
Visiona <darbis@gmail.com>
2016-09-28 20:53:20 +0100

    refactored wording digits method - extension to class Fixnum

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -2,7 +2,7 @@ class Fixnum
 
   def in_words(counter=0)
 
-    numbers_words = {
+    worded_numbers = {
       0 => "zero",
       1 => "one",
       2 => "two",
@@ -41,29 +41,30 @@ class Fixnum
     if self < 0
       puts "No results for negative numbers"
     elsif self == 0
-      numbers_words[0]
+      worded_numbers[0]
     else
-      no_in_progress = self.divmod(1000)
-      no_in_progress = no_in_progress[1]
+      num_sliced = self.divmod(1000)
+      num_sliced = num_sliced[1]
+      hundreds_part, tens_part = num_sliced.divmod(100)
+      last_digit = tens_part.divmod(10)[1]
       phrase = ""
-      if no_in_progress/100 >= 1 && no_in_progress/100 < 10 # 999/100 gives 9 so greater than one
-        phrase += numbers_words[no_in_progress/100] + " " + numbers_words[100]
-        phrase += " " if no_in_progress%100 > 0
-        no_in_progress = no_in_progress%100 #no_in_progress now equals 99
+
+      if (1...10).include? hundreds_part
+        phrase += worded_numbers[hundreds_part] + " " + worded_numbers[100]
+        phrase += " " if tens_part > 0
       end
-      if no_in_progress > 20 && no_in_progress < 100
-        phrase += numbers_words[no_in_progress - no_in_progress%10]
-        phrase += " " + numbers_words[no_in_progress%10] if no_in_progress%10 > 0 
-      elsif no_in_progress <= 20 && no_in_progress > 0
-        phrase += numbers_words[no_in_progress]
+
+      if (21..100).include? tens_part
+        phrase += worded_numbers[tens_part - last_digit]
+        phrase += " " + worded_numbers[last_digit] if last_digit > 0 
+      elsif (1..20).include? tens_part
+        phrase += worded_numbers[tens_part]
       end
+      
       higher_phrase = (self / 1000).in_words(counter + 1) if self >= 1000
-      suffix = numbers_words[1000**counter] if counter > 0 && phrase != ""
-      puts "DBG: suffix = #{suffix.inspect}"
+      suffix = worded_numbers[1000**counter] if counter > 0 && phrase != ""
       "#{higher_phrase} #{phrase} #{suffix}".strip
     end
+
   end
 end
-
-
-
